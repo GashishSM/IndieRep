385db818a3d79926a83bb647664074825d4affe1
Gashish <Gashish@Gashish-PC>
2012-11-17 19:04:43 +0600

    Main

---
+++ IndieProj/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Gashish
+ *
+ * Created on 17 November 2012, 19:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
